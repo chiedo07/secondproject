8b9816b1f00295e50a0851c230064bea3f80eb5a
chiedozie <chiedoluiz@gmail.com>
2022-08-13 01:00:22 +0100

    final job

--- quiz.js
+++ quiz.js
@@ -1,32 +1,32 @@
 const quizQuestions = [
   {
-	 question: "Who invented JavaScript?",
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich",
+	 question: "Which is a recommended quality review team role?",
+      a: "Project Manager",
+      b: "Assurance",
+      c: "Project Support",
+      d: "Administrator",
+    correctAnswer: "d",
+  },
+  {
+    question: "Which is a typical core activity within configuration management?",
+      a: "Quality Assurance",
+      b: "Risk Assurance",
+      c: "Verification And Audit",
       d: "None of the above",
     correctAnswer: "c",
   },
   {
-    question: "Which one of these is a JavaScript package manager?",
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-      d: "None of the above",
-    correctAnswer: "c",
-  },
-  {
-    question: "Which tool can you use to ensure code quality?",
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
+    question: "Which is a characteristic of a project",
+      a: "Low Risk",
+      b: "Fallback",
+      c: "Business as usual",
+      d: "Cross-Functional",
     correctAnswer: "d",
   },
 ];
 
 const quiz = document.getElementById('quiz');
-const answerS1s= document.querySelectorAll('.answer');
+const answerS1s = document.querySelectorAll('.answer');
 const question1 = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -38,11 +38,10 @@ let currentQuiz = 0;
 let score = 0;
 
 
-showQuiz()
 
 function showQuiz() {
 
-  deselectAnswers();
+  deselectAnswers()
 
   const currentQuizData = quizQuestions[currentQuiz];
   question1.innerText = currentQuizData.question;
@@ -51,36 +50,39 @@ function showQuiz() {
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
 }
-function getSelected() {
-let answer
-answerS1s.forEach(answerS1 => {
-  if(answerS1.checked) {
-    answer = answerS1.id
-  }
-})
-return answer
-}
+showQuiz();
+
 function deselectAnswers(){
-answerS1s.forEach (answerS1 => answerS1.checked = false)
+  answerS1s.forEach(answerS1 => answerS1.checked = false)
 }
 
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-  if (answer){
-     if (answer === quizQuestions[currentQuiz].correctAnswer) {
-         score++
-        } 
-
+function getSeleceted(){
+  let answer
+  answerS1s.forEach(answerS1 => {
+    if(answerS1.checked){
+      answer = answerS1.id
+    }
+  })
+  return answer
+}
+ 
+ function setNextQuestion(){
+  const answer = getSeleceted()
+  if(answer){
+    if( answer === quizQuestions[currentQuiz].correctAnswer) {
+      score++; 
+    }
     currentQuiz++
-
-    if(currentQuiz < quizQuestions.length){
+     if(currentQuiz < quizQuestions.length){
       showQuiz()
-    } else{
-     quiz.innerHTML = `
-     <h2> you answered ${score}/${quizQuestions.length} correctly</h2>
-     <button onclick="location.reload()">Reload</button>`;
-   }
-  }
-});
+     }
+     else{
+      quiz.innerHTML = `
+      <p style="text-align:center;margin-top:150px;margin-left:50px;text-align:center;justify-conent:center;font-size:25px;font-weight:bolder;"> you scored ${score}/${quizQuestions.length} </p>
+      <button style="padding-bottom:20px; margin-top:262px;"onclick="location.reload()">reload</button>`
+     }
 
+  }
+}
+ submitBtn.addEventListener('click',setNextQuestion)
 
